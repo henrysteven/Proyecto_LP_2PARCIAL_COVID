8ba1959af9946347fbc17d860cf45b8d6c792845
henstsia <henstsia@espol.edu.ec>
2021-02-01 16:19:34 -0500

    formulario termiando

--- config_db_crud/insert.php
+++ config_db_crud/insert.php
@@ -6,9 +6,10 @@
         $query = "INSERT INTO Fallecidos (cedula_fallecido, nombre_fallecido, apellido_fallecido, sexo, causa_muerte_oficial, caso_sospechoso_covid, cedula) VALUES ('$cedula_fallecido','$nombre_fallecido','$apellido_fallecido','$sexo','$causa_muerte_oficial','$caso_sospechoso_covid','$cedula')";
         $result = mysqli_query($link, $query);
         if ($result){
-            echo "bueno";
+            $cadena = '<div id=mensaje>Datos guardados correctamente</div>';
+            echo $cadena;
         }else{
-            echo "" . mysqli_error($link);
+            echo "<div id=mensaje2>" . mysqli_error($link).'</div>';
         }
         $link -> close();
     } 
@@ -19,13 +20,58 @@
         $query = "INSERT INTO Persona (cedula, nombre, apellido, ciudad, covid, forma_contagio, tuvo_sintomas, sexo, hospitalizado, edad, fecha, sintomas) VALUES ('$cedula','$nombre','$apellido','$ciudad','$covid','$forma_contagio','$tuvo_sintomas','$sexo','$hospitalizados','$edad','$fecha','$sintomas')";
         $result = mysqli_query($link, $query);
         if ($result){
-            echo "bueno";
+            $cadena = '<div id=mensaje>Datos guardados correctamente</div>';
+            echo $cadena;
         }else{
-            echo "" . mysqli_error($link);
+            echo "<div id=mensaje2>" . mysqli_error($link).'</div>';
         }
         $link -> close();
     }
-    insertar_Persona('1650648435','Ruth Leonor','Pluas Lopez','Guayas','no','ninguno','no','F','no','24','2021-01-21','ninguno');        
     //insertar_Fallecido("0985046813","YYY","zzz","M","ninguna","no","0950648139");     
-
+  /*   $linea = 0;
+    //Abrimos nuestro archivo
+    $archivo = fopen("../datos2.csv", "r");
+    //Lo recorremos
+    while (($datos = fgetcsv($archivo, 3000,",")) == true) 
+    {
+        $num = count($datos);
+        $linea++;
+    //Recorremos las columnas de esa linea
+        $nombre = $datos[0];
+        $apellido= $datos[1];
+        $cedula = $datos[2];
+        $covid = $datos[3];
+        $ciudad = $datos[4];
+        $forma_contagio = $datos[5];
+        $tuvo_sintomas = $datos[6];
+        $sexo = $datos[7];
+        $hospitalizados = $datos[8];
+        $edad = $datos[9];
+        $fecha = $datos[10];
+        $sintomas = $datos[11];
+        insertar_Persona($cedula,$nombre,$apellido,$ciudad,$covid,$forma_contagio,$tuvo_sintomas,$sexo,$hospitalizados,$edad,$fecha,$sintomas);
+    }
+    //Cerramos el archivo
+    fclose($archivo); 
+    $linea = 0;
+    //Abrimos nuestro archivo
+    $archivo = fopen("../datos3.csv", "r");
+    //Lo recorremos
+    while (($datos = fgetcsv($archivo, 3000,",")) == true) 
+    {
+        $num = count($datos);
+        $linea++;
+    //Recorremos las columnas de esa linea
+        $cedula_fallecido = $datos[0];
+        $nombre_fallecido= $datos[1];
+        $apellido_fallecido = $datos[2];
+        $sexo = $datos[3];
+        $causa_muerte = $datos[4];
+        $caso_sospechoso = $datos[5];
+        $cedula = $datos[6];
+        insertar_Fallecido($cedula_fallecido,$nombre_fallecido,$apellido_fallecido,$sexo,$causa_muerte,$caso_sospechoso,"0950648139");
+    }
+    //Cerramos el archivo
+    fclose($archivo);
+    */
 ?>
--- formulario/funciones_disenio_frontend.php
+++ formulario/funciones_disenio_frontend.php
@@ -4,7 +4,7 @@
                     "Orellana","Pastaza","Pichincha","Santa Elena","Santo Domingo de los Tsáchilas",
                     "Sucumbíos","Tungurahua","Zamora Chinchipe");
     $doc = new DOMDocument();
-    $doc -> loadHTMLFile("vistas/formulario.html");
+    $doc -> loadHTMLFile("../vistas/formulario.html");
     $selector = $doc->getElementById("selector-ciudad");
     foreach($ciudades as $ciudad){
         $tag_option = $doc -> createElement('option');
@@ -14,4 +14,27 @@
     }
     /* $doc ->  */
     print $doc->saveHTML($doc);
+    include("../config_db_crud/insert.php"); 
+    if($_POST){
+        $cedula = $_POST["cedula"];
+        $nombres = $_POST["nombres"];
+        $apellidos = $_POST["apellidos"];
+        $ciudad = $_POST["ciudad"];
+        $covid = $_POST["tuvo_covid"];
+        $forma_contagio = $_POST["forma_contagio"];
+        $tuvo_sintomas = "";
+        $sintomas = "";
+        if($_POST["check"] ){
+            $tuvo_sintomas = "si";
+            $sintomas = implode(",",$_POST["check"]);
+        }else{
+            $tuvo_sintomas = "no";
+        }
+        $sexo = $_POST["sexo"];
+        $hospitalizado = $_POST["hospitalizado"];
+        $edad = $_POST["edad"];
+        $fecha = date('Y-m-d');
+        insertar_Persona($cedula,$nombres,$apellidos,$ciudad,$covid,$forma_contagio,$tuvo_sintomas,$sexo,$hospitalizado,$edad,$fecha,$sintomas);
+            
+    }
 ?>
